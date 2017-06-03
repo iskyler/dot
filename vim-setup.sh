6f3340b2b901486cde18376648dd05fa9740534b
skyler <iskyler1993@gmail.com>
2017-06-04 02:12:09 +0800

    avoid open vim window in shell

--- vim-setup.sh
+++ vim-setup.sh
@@ -41,7 +41,7 @@ config_vundle() {
     echo "Vundle install ok"
     hash vim 2>/dev/null || { \
         echo "vim command not found"; exit -1; }
-    vim +BundleInstall +qall
+    vim -E +BundleInstall +qall
 }
 
 link
